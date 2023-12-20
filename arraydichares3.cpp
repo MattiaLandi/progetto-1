26c75522dfa464d3be3555978c4051e4d58c740b
MattiaLandi <147489259+MattiaLandi@users.noreply.github.com>
2023-12-20 12:14:08 +0100

    Create arraydichares3.cpp

---
+++ arraydichares3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int contaVocali( char* str) {
+    int contatore = 0;
+    int lunghezza ;
+
+    for (int i = 0; i < lunghezza; ++i) {
+       
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+            contatore++;
+        }
+    }
+
+    return contatore;
+}
+
+int main() {
+     int lunghezzaMassima = 100;
+    char input[lunghezzaMassima];
+
+    
+   cout << "Inserisci una stringa: ";
+   cin>>input ;
+
+    
+    int numeroVocali = contaVocali(input);
+   cout << "Il numero di vocali nella stringa inserita e': " << numeroVocali << std::endl;
+
+    return 0;
+}
