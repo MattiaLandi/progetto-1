1a22c83a68039d06becadcd8cc8f702e66302734
MattiaLandi <147489259+MattiaLandi@users.noreply.github.com>
2024-01-05 11:59:21 +0100

    Create array chares6.cpp

---
+++ array chares6.cpp
@@ -0,0 +1,21 @@
+include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(){
+string fortnite;
+string minecraft;
+cout<<"inserire 1 stringa"<<endl;
+cin>>fortnite;
+cout<<"inserire 2 stringa"<<endl;
+cin>>minecraft;
+if(minecraft.length()>fortnite.length()){
+cout<<"la stringa piu lunga è: "<<minecraft<<endl;}
+else{
+    cout<<"la stringa piu lunga è: "<<fortnite<<endl;
+}
+
+    
+}
+
